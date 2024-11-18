532e5eaf52f848a0212ccf04c3f8c065bda88f5f
SarinaMurakami <sa-murakami@clinks.jp>
2024-11-18 17:51:57 +0900

    instance

---
+++ Section5/object/instance.php
@@ -0,0 +1,45 @@
+<?php
+
+class Product{
+
+    //アクセス修飾子, private(外からアクセスできない), protected(自分と継承したクラス), public(公開)
+
+    //変数
+    private $product = [];
+
+    //関数
+    //classを呼び出すときの初回の関数「__construst」
+    function __construct($product = null){
+
+        $this->product = $product;
+    }
+
+    public function getProduct(){
+        echo $this->product;
+    }
+
+    public function addProduct($item){
+        $this->product .= $item;
+    }
+
+    public static function getStaticProduct($str){
+        echo $str;
+    }
+
+}
+
+$instance = new Product('テスト');
+
+var_dump($instance);
+
+$instance->getProduct();
+echo'<br>';
+
+$instance->addProduct('追加分');
+echo'<br>';
+
+$instance->getProduct();
+echo '<br>';
+
+Product::getStaticProduct('静的');
+echo'<br>';
