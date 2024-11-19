bd762c7254052d3758a0008953c42162d7fdb4a4
SarinaMurakami <sa-murakami@clinks.jp>
2024-11-19 15:24:15 +0900

    composer_autoload

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0295ff60392a2d122fecd3457681fbb70250d1e9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0295ff60392a2d122fecd3457681fbb70250d1e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
